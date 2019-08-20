24ffd6cb77903bfa8611b6ef5e0204dec7e659c8
Borna Bešić <besic.borna@gmail.com>
2019-08-20 09:41:58 +0200

    Add setup.py (setuptools)

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="pyromaniac",
+    author="Borna Bešić",
+    description="Reload modules and classes and live-patch objects during runtime",
+    packages=find_packages(),
+)
+
